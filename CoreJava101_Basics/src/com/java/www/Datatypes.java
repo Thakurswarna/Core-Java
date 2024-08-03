820dbffa123696031175233ce3384e6d2f9f9687
swarna rekha <swarna rekha@LAPTOP-45ANVT6G>
2024-08-03 09:45:31 +0530

    First commit

---
+++ CoreJava101_Basics/src/com/java/www/Datatypes.java
@@ -0,0 +1,45 @@
+package com.java.www;
+
+public class Datatypes {
+	
+	//Java contains 8 primitive datatypes
+	//Numbe: byte,short,int,long
+	//floating point numbers: float,double
+	//character: char
+	//boolean:boolean
+	
+	public static void main(String[] args) {
+		
+		byte b1 = 10;
+		short s1 = 20;
+		int i1 = 30;
+		long l1 = 40;
+		
+		System.out.println(b1);
+		System.out.println(s1);
+		System.out.println(i1);
+		System.out.println(l1);
+		
+		//Variables holds the value and stores the value
+		//Variables = b1,s1,i1,l1
+		//Datatypes = Byte,Short,Int,Long
+		//= Is operator
+		// Values = 10,20,30,40
+		
+		float f1 = 10.0f;
+		double d1 =20.0;
+		char ch = 'A';
+		System.out.println(f1);
+		System.out.println(d1);
+		System.out.println(ch);
+		
+		Boolean b11 = true;
+		System.out.println(b11);
+		
+		
+	
+		
+		
+	}
+
+}
---
+++ CoreJava101_Basics/src/com/java/www/Defaultvalues.java
@@ -0,0 +1,31 @@
+package com.java.www;
+
+public class Defaultvalues {
+	
+	static byte b1;
+	static short s1;
+	static int i1;
+	static long l1;
+	static float f1;
+	static double d1;
+	static char ch;
+	static boolean b11;
+	
+	public static void main(String[] args) {
+		
+		  
+		
+		System.out.println(b1); //0
+		System.out.println(s1); //0
+		System.out.println(i1); //0
+		System.out.println(l1); //0
+		System.out.println(f1); //0.0
+		System.out.println(d1); //0.0
+		System.out.println(ch); //space
+		System.out.println(b11); //false
+		
+		
+		
+	}
+
+}
