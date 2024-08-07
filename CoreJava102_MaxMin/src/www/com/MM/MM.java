5009889c447893684217a590b8d26bdf5cd36e80
swarna rekha <swarna rekha@LAPTOP-45ANVT6G>
2024-08-07 08:10:29 +0530

    Commit

---
+++ CoreJava102_MaxMin/src/www/com/MM/MM.java
@@ -0,0 +1,10 @@
+package www.com.MM;
+
+public class MM {
+public static void main(String[] args) {
+	System.out.println(Byte.MAX_VALUE);
+	System.out.println(Byte.MIN_VALUE);
+	
+	System.out.println(Double.MAX_VALUE);
+}
+}
